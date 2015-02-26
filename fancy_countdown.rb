623018261942befc1e10effd36c33f17b212de01
Kiersten Krajcar <kierstenella@gmail.com>
2015-02-25 20:14:10 -0800

    added weeks left in year and leftover days

--- fancy_countdown.rb
+++ fancy_countdown.rb
@@ -4,7 +4,13 @@ def print_days_until_event(label, year, month, day)
   event_date = Date.new(year, month, day)
   days_until_event_date = (event_date - Date.today).to_i
   puts "#{label} is in #{days_until_event_date} days"
+  return days_until_event_date
 end
 
 print_days_until_event("Kiersten's birthday", 2015, 3, 3)
 print_days_until_event("Sophie's birthday", 2015, 4, 17)
+print_days_until_event("Tim's birthday", 2015, 8, 9)
+days_until_end_of_year = print_days_until_event("December 31st", 2015, 12, 31)
+
+puts (days_until_end_of_year/7).to_s + " weeks remain in the year."
+puts (days_until_end_of_year%7).to_s + " days left over."
